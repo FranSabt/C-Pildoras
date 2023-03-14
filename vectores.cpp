684dfd9b8b0cd3697aa198c65ccdadb906aadabe
FranSabt <hsabatino1@gmail.com>
2023-03-13 23:41:21 -0400

    vectores

---
+++ vectores.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//* Es una clase de la libreria c++
+//* Crecen y decrecen de manera dinamcia
+//* Los elementos almacenados son del mismo tipo
+int main () 
+{
+    vector <int> records;
+    vector <int> edades (5); //* le pone limite
+    vector <char> chars;
+}
