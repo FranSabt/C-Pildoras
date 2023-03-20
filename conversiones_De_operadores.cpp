29a6b479a0f7bdf65f7df7242725cbc8958d65cc
FranSabt <hsabatino1@gmail.com>
2023-03-20 14:15:31 -0400

    conversiones de operadores aritmeticos

---
+++ conversiones_De_operadores.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int     a = 13;
+    int     b = 7;
+    int     c = 6;
+    double     d = 7;
+
+    double r = (a + b + c) / d;
+    cout << r << endl;
+}
