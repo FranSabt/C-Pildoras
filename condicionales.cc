51dabf6b9d8b5b61bea705457805e06ef170b1b1
FranSabt <hsabatino1@gmail.com>
2023-04-27 22:42:01 -0400

    condicionales 2

--- condicionales.cc
+++ condicionales.cc
@@ -1,14 +1,41 @@
 #include <iostream>
 using namespace std;
 
+
+void toLowerCase(string &cadena)
+{
+    for (int i = 0; i < cadena.length(); i++)
+    {
+        cadena[i] = tolower(cadena[i]);
+    }
+}
+
 int main()
 {
     int edad;
+    string ex_medico;
+    bool aprovado;
+
     cout << "Por favor introduce tu edad" << endl;
 
     cin >> edad ;
 
-    if(edad >= 18)
+    cout << "Paso el examen médico? (Si/No): ";
+    cin >> ex_medico;
+
+    toLowerCase(ex_medico);
+
+    if(ex_medico == "si" || ex_medico == "s" )
+    {
+        aprovado = 1;
+    }
+
+    else
+    {
+        aprovado = 0;
+    }
+
+    if(edad >= 18 && aprovado)
     {
         cout << "Puedes conducir" << endl;
     }
