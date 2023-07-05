56d4f933da7da35fee2aeadecee8e20fcf1dcbcf
FranSabt <hsabatino1@gmail.com>
2023-07-04 22:59:04 -0400

    piramide con bucles

--- bucles.cpp
+++ bucles.cpp
@@ -5,4 +5,20 @@ int main ()
 {
     cout << "Hola Mundo!" << endl;
 
+    int size;
+
+    cout << "Tamaño de la piramide?: ";
+
+    cin >> size;
+
+    for (int i = 0; i < size; i ++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            cout << "#";
+        }
+        cout << "\n";
+    }
+
+
 }
